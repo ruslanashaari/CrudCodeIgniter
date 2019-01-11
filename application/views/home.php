ef2e5bd3bad995e0b3903c8e44635ba04eb27daf
ruslanashaari <ruslan.ashaari@gmail.com>
2019-01-11 17:52:50 +0800

    base code

---
+++ application/views/home.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+	<title>Create</title>
+</head>
+<body>
+	<br>
+	<div class="container">
+		<div class="row">
+			<div class="card" style="width: 18rem;">
+			  <div class="card-body">
+			    <h5 class="card-title">Create</h5>
+			    <h6 class="card-subtitle mb-2 text-muted">Create and insert data</h6>
+			    <p class="card-text">Some implementation of inserting data into mysql database.</p>
+			    <a href="#" class="card-link">Click Here</a>
+			  </div>
+			</div>
+		</div>
+	</div>
+
+    <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-3.3.1.min.js"); ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+</body>
+</html>
