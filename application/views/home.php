7e2b84b75956410498dccf3bd6425ff859cae83d
ruslanashaari <ruslan.ashaari@gmail.com>
2019-01-11 19:15:31 +0800

    updating landing views

--- application/views/home.php
+++ application/views/home.php
@@ -1,28 +1,37 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
-	<title>Create</title>
-</head>
-<body>
-	<br>
-	<div class="container">
-		<div class="row">
-			<div class="card" style="width: 18rem;">
-			  <div class="card-body">
-			    <h5 class="card-title">Create</h5>
-			    <h6 class="card-subtitle mb-2 text-muted">Create and insert data</h6>
-			    <p class="card-text">Some implementation of inserting data into mysql database.</p>
-			    <a href="#" class="card-link">Click Here</a>
-			  </div>
-			</div>
-		</div>
+<div class="row">
+	<div class="card" style="width: 18rem; margin: 10px;">
+	  <div class="card-body">
+	    <h5 class="card-title">Create</h5>
+	    <h6 class="card-subtitle mb-2 text-muted">Create data</h6>
+	    <p class="card-text">Some implementation of inserting data into mysql database.</p>
+	    <a href="#" class="btn btn-primary">Click Here</a>
+	  </div>
 	</div>
 
-    <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-3.3.1.min.js"); ?>"></script>
-	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
-</body>
-</html>
+	<div class="card" style="width: 18rem; margin: 10px;">
+	  <div class="card-body">
+	    <h5 class="card-title">Read</h5>
+	    <h6 class="card-subtitle mb-2 text-muted">Display data</h6>
+	    <p class="card-text">Some implementation for displaying data from mysql database.</p>
+	    <a href="#" class="btn btn-warning">Click Here</a>
+	  </div>
+	</div>
+
+	<div class="card" style="width: 18rem; margin: 10px;">
+	  <div class="card-body">
+	    <h5 class="card-title">Update</h5>
+	    <h6 class="card-subtitle mb-2 text-muted">Update data</h6>
+	    <p class="card-text">Some implementation for updating data from mysql database.</p>
+	    <a href="#" class="btn btn-success">Click Here</a>
+	  </div>
+	</div>
+
+	<div class="card" style="width: 18rem; margin: 10px;">
+	  <div class="card-body">
+	    <h5 class="card-title">Delete</h5>
+	    <h6 class="card-subtitle mb-2 text-muted">Delete data</h6>
+	    <p class="card-text">Some implementation of deleting data from mysql database.</p>
+	    <a href="#" class="btn btn-danger">Click Here</a>
+	  </div>
+	</div>
+</div>
